310ab3e818d76bff96af850004ad022cda4bff84
juafeis <jmfel@DESKTOP-H66P0LT>
2020-01-10 15:03:37 +0100

    Basic layout implemented

--- DIM_UWP/DIM_UWP/MainPage.xaml.cs
+++ DIM_UWP/DIM_UWP/MainPage.xaml.cs
@@ -17,14 +17,53 @@ using Windows.UI.Xaml.Navigation;
 
 namespace DIM_UWP
 {
+    public class Action
+    {
+        public string MouseAction { get; set; }
+        public string Description { get; set; }
+    }
+
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static List<Action> actions;
         public MainPage()
         {
             this.InitializeComponent();
+            actions = new List<Action>();
+        }
+
+        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            
+            double x = e.GetPosition(image).X;
+            double y = e.GetPosition(image).Y;
+            actions.Add(new Action { MouseAction = "Image_Tapped", Description = "Values: x = " + x.ToString() + " and y = " + y.ToString() });
+            listView.ItemsSource = actions;
+            
+            
+        }
+
+        private void Image_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            double x = e.GetPosition(image).X;
+            double y = e.GetPosition(image).Y;
+        }
+
+        private void Image_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            double x = e.GetPosition(image).X;
+            double y = e.GetPosition(image).Y;
+        }
+
+        private void Image_Holding(object sender, HoldingRoutedEventArgs e)
+        {
+            double x = e.GetPosition(image).X;
+            double y = e.GetPosition(image).Y;
         }
     }
 }
