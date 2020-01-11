4ffceb601a85295b06fe4f1ecded92a23555ec06
Juanfeis <jmfelix@hotmail.com>
2020-01-11 17:25:16 +0100

    Activity4 correctly implemented

--- DIM_UWP/DIM_UWP/Activities/Activity4.xaml.cs
+++ DIM_UWP/DIM_UWP/Activities/Activity4.xaml.cs
@@ -23,53 +23,19 @@ namespace DIM_UWP.Activities
     /// </summary>
     public sealed partial class Activity4 : Page
     {
-        ObservableCollection<Action> Actions { get; set; }
+        List<CompositeTransform> Images { get; set; }
         public Activity4()
         {
             this.InitializeComponent();
-            Actions = new ObservableCollection<Action>();
-            listView.ItemsSource = Actions;
-            image_Transform.ScaleX = image_Transform.ScaleY = 1;
-            image_Transform.Rotation = 0;
-            image.ManipulationMode = ManipulationModes.All;
-        }
-
-        private void AddAction(string methodName, double x, double y)
-        {
-            var action = new Action
+            Images.AddRange(new List<CompositeTransform>{ image_Transform, image_Transform2, image_Transform3, image_Transform4});
+            foreach (var imageT in Images)
             {
-                MouseAction = methodName,
-                Description = "Values: x = " + x.ToString() + " and y = " + y.ToString()
-            };
-            Actions.Add(action);
-        }
-        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            double x = e.GetPosition(image).X;
-            double y = e.GetPosition(image).Y;
-            AddAction("Image_Tapped", x, y);
+                imageT.ScaleX = image_Transform.ScaleY = 1;
+                imageT.Rotation = 0;
+            }
+            
         }
 
-        private void Image_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
-        {
-            double x = e.GetPosition(image).X;
-            double y = e.GetPosition(image).Y;
-            AddAction("Image_DoubleTapped", x, y);
-        }
-
-        private void Image_RightTapped(object sender, RightTappedRoutedEventArgs e)
-        {
-            double x = e.GetPosition(image).X;
-            double y = e.GetPosition(image).Y;
-            AddAction("Image_RightTapped", x, y);
-        }
-
-        private void Image_Holding(object sender, HoldingRoutedEventArgs e)
-        {
-            double x = e.GetPosition(image).X;
-            double y = e.GetPosition(image).Y;
-            AddAction("Image_Holding", x, y);
-        }
 
         private void Image_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
@@ -82,9 +48,6 @@ namespace DIM_UWP.Activities
 
             image_Transform.TranslateX += e.Delta.Translation.X;
             image_Transform.TranslateY += e.Delta.Translation.Y;
-
-
-            AddAction("Manipulation_Delta", image_Transform.ScaleX, image_Transform.ScaleY);
         }
 
         private void Image_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
@@ -96,6 +59,52 @@ namespace DIM_UWP.Activities
         {
             image.Opacity = 1;
         }
-        
+
+        private void Image2_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            image2.Opacity = 1;
+        }
+
+        private void Image2_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image2_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            image2.Opacity = 0.4;
+        }
+
+        private void Image3_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            image3.Opacity = 1;
+        }
+
+        private void Image3_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image3_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            image3.Opacity = 0.4;
+        }
+
+        private void Image4_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            image4.Opacity = 1;
+        }
+
+        private void Image4_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image4_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            image4.Opacity = 0.4;
+        }
+
+
     }
 }
